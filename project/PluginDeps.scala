538cf3a8eef03cedde709f3d2c461e13f73495e2
andrew <uandrew1965@gmail.com>
2017-09-04 19:09:39 +0300

    .

--- project/PluginDeps.scala
+++ project/PluginDeps.scala
@@ -2,14 +2,20 @@ import sbt.addSbtPlugin
 
 import sbt._
 
-object CommonDeps {
+object PluginDeps {
     object versions {
         val pluginVersion = "0.3.3-SNAPSHOT"
         val snVersion = "0.3.2"
         val sjsVersion = "0.6.20"
     }
 
-    val scalaJSPlugin = addSbtPlugin("org.scala-js" % "sbt-scalajs" %  versions.sjsVersion)
+    val scalaJSPlugin = addSbtPlugin("org.scala-js" % "sbt-scalajs" % versions.sjsVersion)
     val scalaJSCrossProjectPlugin = addSbtPlugin("org.scala-native" % "sbt-scalajs-crossproject" % versions.pluginVersion)
-    val scalaCrossProjectPlugin =addSbtPlugin("org.scala-native" % "sbt-crossproject" % versions.pluginVersion)
+    val scalaCrossProjectPlugin = addSbtPlugin("org.scala-native" % "sbt-crossproject" % versions.pluginVersion)
+}
+
+object CommonDeps {
+    object versions {
+
+    }
 }
