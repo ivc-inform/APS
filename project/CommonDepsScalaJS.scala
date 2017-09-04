ed134f915abc2717e1d083651144f68a7078542b
andrew <uandrew1965@gmail.com>
2017-09-04 15:43:57 +0300

    .

---
+++ project/CommonDepsScalaJS.scala
@@ -0,0 +1,14 @@
+import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
+import sbt._
+
+object CommonDepsScalaJS {
+
+    val macroJS = Def.setting("com.simplesys" %%% "macrojs" % Versions.scalaJSVersion)
+    val smartClientWrapper = Def.setting("com.simplesys" %%% "smartclient-wrapper" % Versions.scalaJSVersion)
+    //val smartClientWrapper = Def.setting("com.simplesys" %%% "common-types" % Versions.scalaJSVersion)
+
+    val uPickleJS = Def.setting("com.lihaoyi" %%% "upickle" % Versions.uPickleVersion)
+    val scalaTags = Def.setting("com.lihaoyi" %%% "scalatags" % Versions.scalaTagsVersion)
+    val scalaDom = Def.setting("org.scala-js" %%% "scalajs-dom" % Versions.scalaDomVersion)
+    val jQuery = Def.setting("be.doeraene" %%% "scalajs-jquery" % Versions.jQueryVersion)
+}
---
+++ project/Versions.scala
@@ -0,0 +1,11 @@
+
+object Versions {
+    val scalaJSVersion = "1.4-SNAPSHOT"
+    val scalaTestVersion = "3.0.3"
+    val scalaTagsVersion = "0.6.5"
+    val scalaDomVersion = "0.9.4-SNAPSHOT"
+    val jQueryVersion = "0.9.1"
+    val uPickleVersion = "0.4.4"
+    val scalajsDOMVersion = "0.9.1"
+    val scalajsJQueryVersion = "0.9.0"
+}
