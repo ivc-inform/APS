8ebd5654867bf126439375f5063691388eb979c0
andrew <uandrew1965@gmail.com>
2017-09-05 14:19:28 +0300

    .

--- webUI/.js/src/main/scala/com/ivcInform/app/http/ApsWindowMain.scala
+++ webUI/.js/src/main/scala/com/ivcInform/app/http/ApsWindowMain.scala
@@ -3,6 +3,7 @@ package com.ivcInform.app.http
 import com.simplesys.SmartClient.App.{SettingsEditor, WebTabSetApp}
 import com.simplesys.SmartClient.App.props.SettingsEditorProps
 import com.simplesys.SmartClient.System.SettingsEditor
+import com.simplesys.System.jSUndefined
 import com.simplesys.function._
 import com.simplesys.option.ScOption._
 
@@ -13,19 +14,19 @@ class ApsWindowMain extends WebTabSetApp {
     override protected val identifier = "5814FE1C-252A-01C4-11A1-557FA3345D3F"
     override protected val appImageDir = "images/"
 
-    override protected val dataSourcesJS_admin_UserGroup_DS = _
-    override protected val dataSourcesJS_admin_User_DS = _
+    override protected val dataSourcesJS_admin_UserGroup_DS = jSUndefined
+    override protected val dataSourcesJS_admin_User_DS = jSUndefined
 
-    override protected val listGridFiledsJS_admin_UserGroup_FLDS = _
-    override protected val listGridFiledsJS_admin_User_FLDS = _
+    override protected val listGridFiledsJS_admin_UserGroup_FLDS = Seq()
+    override protected val listGridFiledsJS_admin_User_FLDS = Seq()
 
-    override protected val formItemsJS_admin_UserGroup_FRMITM = _
-    override protected val formItemsJS_admin_User_FRMITM = _
+    override protected val formItemsJS_admin_UserGroup_FRMITM = Seq()
+    override protected val formItemsJS_admin_User_FRMITM = Seq()
 
-    override protected val admin_User_codeGroup_NameStrong = _
+    override protected val admin_User_codeGroup_NameStrong = jSUndefined
 
-    override protected val managedUsersGroups = _
-    override protected val progectManagedDevsGroups = _
+    override protected val managedUsersGroups = Seq()
+    override protected val progectManagedDevsGroups = Seq()
 
     override protected def getSettingsEditor(): SettingsEditor = SettingsEditor.create(
         new SettingsEditorProps {
