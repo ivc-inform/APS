011775bfcb097921fcd8c953447d4dc097a36b12
andrew <uandrew1965@gmail.com>
2017-09-13 07:07:15 +0300

    .

--- web-ui/src/main/scala/com/simplesys/container/StartPageContainer.scala
+++ web-ui/src/main/scala/com/simplesys/container/StartPageContainer.scala
@@ -13,7 +13,7 @@ class StartPageContainer(val request: HttpServletRequest, val response: HttpServ
 
     def receive = {
         case GetData => {
-            val textHTML = new StartPage("Расписание".ellipsis, scalatags.Text)
+            val textHTML = new StartPage("Расписание для одиночной машины".ellipsis, scalatags.Text)
 
             val html: String = "<!DOCTYPE html>" +
               textHTML.bodyHTML(
