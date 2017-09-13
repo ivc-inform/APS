8f87b5822f7158b5634fe1a018b9013a19af8956
andrew <uandrew1965@gmail.com>
2017-09-13 20:45:59 +0300

    .

--- web-ui/src/main/scala/com/simplesys/container/SemiHandTraits/math_result_items_SemiHandTrait_Remove.scala
+++ web-ui/src/main/scala/com/simplesys/container/SemiHandTraits/math_result_items_SemiHandTrait_Remove.scala
@@ -1,4 +1,4 @@
-// This file is generated automatically (at 12.09.2017 22:37:57), do not spend any changes here, because they will be lost. Generator: "GenBOContainer, stage: #765"
+// This file is generated automatically (at 13.09.2017 20:34:26), do not spend any changes here, because they will be lost. Generator: "GenBOContainer, stage: #765"
 
 package ru.simplesys.defs.app.scala.container.math
 
@@ -11,14 +11,14 @@ import akka.actor.Actor
 import ru.simplesys.defs.bo.math._
 
  
-trait math_result_item_SemiHandTrait_Fetch extends SessionContextSupport with ServletActorDyn {
+trait math_result_items_SemiHandTrait_Remove extends SessionContextSupport with ServletActorDyn {
     
 /////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!!!! DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
     val requestData = new DSRequestDyn(request)    
     
-    logger debug s"Request for Fetch: ${newLine + requestData.toPrettyString}"    
+    logger debug s"Request for Remove: ${newLine + requestData.toPrettyString}"    
     
-    val dataSet = Result_itemDS(oraclePool)    
+    val dataSet = Result_itemsDS(oraclePool)    
 /////////////////////////////// !!!!!!!!!!!!!!!!!!!!!!!!!! END DON'T MOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ///////////////////////////////    
     
      def receiveBase: Option[Actor.Receive] = None    
