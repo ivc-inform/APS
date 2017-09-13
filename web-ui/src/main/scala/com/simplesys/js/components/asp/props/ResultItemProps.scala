b15a1df0eaf9cdc9bfc9b278800ec1bf354e11a6
andrew <uandrew1965@gmail.com>
2017-09-13 19:21:23 +0300

    .

--- web-ui/src/main/scala/com/simplesys/js/components/asp/props/ResultItemProps.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/asp/props/ResultItemProps.scala
@@ -1,15 +1,26 @@
 package com.simplesys.js.components.asp.props
 
-import com.simplesys.SmartClient.App.props.CommonListGridEditorComponentProps
+import com.simplesys.SmartClient.App.props._
 import com.simplesys.SmartClient.System._
+import com.simplesys.System.Types.ListGridEditEvent
 import com.simplesys.System._
 import com.simplesys.function._
 import com.simplesys.js.components.asp.ResultItem
 import com.simplesys.option.ScOption._
+import ru.simplesys.defs.app.gen.scala.ScalaJSGen.{DataSourcesJS, FormItemsJS, ListGridFiledsJS}
 
 import scala.scalajs.js._
 
 class ResultItemProps extends CommonListGridEditorComponentProps {
     type classHandler <: ResultItem
     identifier = "4EEF794F-EE8F-C38D-73D6-CF6F0F5170E3".opt
+
+    dataSource = DataSourcesJS.math_result_item_DS.opt
+
+    editingFields = FormItemsJS.math_result_item_FRMITM.opt
+    fields = ListGridFiledsJS.math_result_item_FLDS.opt
+
+    editEvent = ListGridEditEvent.none.opt
+
+    itemsType = Seq(miNewWithForm(false), miCopy(false), miDelete(false), miEdit(false), miRefresh()).opt
 }
--- web-ui/src/main/scala/com/simplesys/js/components/asp/props/ResultProps.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/asp/props/ResultProps.scala
@@ -1,23 +1,51 @@
 package com.simplesys.js.components.asp.props
 
-import com.simplesys.SmartClient.App.props.CommonListGridEditorComponentProps
-import com.simplesys.SmartClient.System._
-import com.simplesys.System._
+import com.simplesys.SmartClient.App.props._
+import com.simplesys.SmartClient.DataBinding.props.{AdvancedCriteriaProps, CriterionProps}
+import com.simplesys.SmartClient.System.{AdvancedCriteria, Criterion}
+import com.simplesys.System.JSAny
+import com.simplesys.System.Types.{ListGridEditEvent, OperatorId}
+import com.simplesys.app.ResultItem
 import com.simplesys.function._
 import com.simplesys.js.components.asp.Result
 import com.simplesys.option.ScOption._
-import ru.simplesys.defs.app.gen.scala.ScalaJSGen.{DataSourcesJS, FormItemsJS, ListGridFiledsJS}
-
-import scala.scalajs.js._
+import ru.simplesys.defs.app.gen.scala.ScalaJSGen._
+import ru.simplesys.defs.app.scala.container.math.ResultDataRecord
 
 class ResultProps extends CommonListGridEditorComponentProps {
     type classHandler <: Result
     identifier = "4EEF794F-EE8F-C38D-73D6-CF6F0F5170E5".opt
 
-    simpleTable = true.opt
+    //simpleTable = true.opt
 
     dataSource = DataSourcesJS.math_result_DS.opt
 
     editingFields = FormItemsJS.math_result_FRMITM.opt
     fields = ListGridFiledsJS.math_result_FLDS.opt
+
+    editEvent = ListGridEditEvent.none.opt
+    canExpandRecords = true.opt
+
+    itemsType = Seq(miNewWithForm(false), miCopy(false), miDelete(false), miEdit(false), miRefresh()).opt
+
+    getExpansionComponent = {
+        (thiz: classHandler, record: ResultDataRecord) ⇒
+            ResultItem.create(
+                new ResultItemProps {
+                    initialCriteria = AdvancedCriteria(
+                        new AdvancedCriteriaProps {
+                            operator = OperatorId.and.opt
+                            criteria = Seq(
+                                Criterion(
+                                    new CriterionProps {
+                                        fieldName =  math_result_id_NameStrong.name.opt
+                                        operator = OperatorId.equals.opt
+                                        value = record.id.getOrElse(0).asInstanceOf[JSAny].opt
+                                    })
+                            ).opt
+                        }
+                    ).opt
+                }
+            )
+    }.toThisFunc.opt
 }
