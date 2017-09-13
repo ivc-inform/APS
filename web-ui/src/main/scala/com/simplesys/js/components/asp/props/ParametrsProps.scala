9bab8888ade62f5c7341dcedd806ff78e49fe507
andrew <uandrew1965@gmail.com>
2017-09-13 17:20:00 +0300

    .

--- web-ui/src/main/scala/com/simplesys/js/components/asp/props/ParametrsProps.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/asp/props/ParametrsProps.scala
@@ -1,11 +1,13 @@
 package com.simplesys.js.components.asp.props
 
 import com.simplesys.SmartClient.App.props.CommonListGridEditorComponentProps
+import com.simplesys.SmartClient.Layout.props.WindowSSProps
 import com.simplesys.SmartClient.System._
 import com.simplesys.System._
 import com.simplesys.function._
 import com.simplesys.js.components.asp.Parametrs
 import com.simplesys.option.ScOption._
+import com.simplesys.option.DoubleType._
 import ru.simplesys.defs.app.gen.scala.ScalaJSGen.{DataSourcesJS, FormItemsJS, ListGridFiledsJS}
 
 import scala.scalajs.js._
@@ -14,10 +16,17 @@ class ParametrsProps extends CommonListGridEditorComponentProps {
     type classHandler <: Parametrs
     identifier = "4EEF794F-EE8F-C38D-73D6-CF6F0F5170E4".opt
 
-    simpleTable = true.opt
+    //simpleTable = true.opt
 
     dataSource = DataSourcesJS.math_params_DS.opt
 
     editingFields = FormItemsJS.math_params_FRMITM.opt
     fields = ListGridFiledsJS.math_params_FLDS.opt
+
+    editWindowProperties = WindowSS(
+        new WindowSSProps {
+            width = 285
+            height = 500
+        }
+    ).opt
 }
