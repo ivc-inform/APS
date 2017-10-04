6f2b3d3e77d234a83219e58874440c79b8a355d8
andrew <uandrew1965@gmail.com>
2017-10-04 18:37:44 +0300

    .

--- web-ui/src/main/scala/com/simplesys/js/components/gantt/DateUtils.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/gantt/DateUtils.scala
@@ -1,7 +1,21 @@
 package com.simplesys.js.components.gantt
 
 import scala.scalajs.js
+import scala.scalajs.js.Date
+
+trait BoundaryDatesFromData extends js.Object {
+    val minStart: Date
+    val maxEnd: Date
+}
 
 class DateUtils extends js.Object {
+    def getBoundaryDatesFromData(data: js.Array[_ <: DataStructItem], minDays: Int): BoundaryDatesFromData = {
+        val _minStart = new Date()
+        val _maxEnd = new Date()
 
+        new BoundaryDatesFromData {
+            override val maxEnd = _maxEnd
+            override val minStart = _minStart
+        }
+    }
 }
