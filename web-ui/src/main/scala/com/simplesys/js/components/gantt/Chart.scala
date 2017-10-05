d10c063f83da2387e1967c193096a0e646a49f3d
andrew <uandrew1965@gmail.com>
2017-10-05 13:43:46 +0300

    .

--- web-ui/src/main/scala/com/simplesys/js/components/gantt/Chart.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/gantt/Chart.scala
@@ -14,44 +14,50 @@ class Chart(div: JQuery, opts: GanttChartOptions) extends js.Object {
     def render(): Unit = {
         opts.data.foreach(data ⇒ opts.cellHeight.foreach(addVtHeader(div, data, _)))
 
-        val slideDiv = jQuery("<div>", new js.Object {
-            val `class` = "ganttview-slide-container"
-            val `css` = new js.Object {
-                val `width` = opts.slideWidth + "px"
+        val slideDiv = {
+            val _css = new js.Object {
+                val width = opts.slideWidth.get + "px"
             }
-        })
 
-        val dates = opts.start.map{
+            jQuery("<div>", new js.Object {
+                val `class` = "ganttview-slide-container"
+                val css = _css
+            })
+        }
+
+        isc debugTrap slideDiv
+
+        val dates = opts.start.map {
             start ⇒
-                opts.end.map{
+                opts.end.map {
                     end ⇒
                         getDates(start, end)
                 }
         }
     }
 
-    def getDates(start:Date, end: Date): js.Array[js.Array[js.Array[Date]]] = {
-        
+    def getDates(start: Date, end: Date): js.Array[js.Array[js.Array[Date]]] = {
+
         val dates = js.Array[js.Array[js.Array[Date]]]()
         dates.update(start.getFullYear(), js.Array[js.Array[Date]]())
         dates(start.getFullYear()).update(start.getMonth(), js.Array(start))
 
-        isc debugTrap dates
+        //isc debugTrap dates
 
         var last = start
-        while (last.getTime() < end.getTime()) {
+        /*while (last.getTime() < end.getTime()) {
             val next = isc.clone(last).addDays(1)
-        }
+        }*/
         dates
     }
 
     def addVtHeader(div: JQuery, data: js.Array[_ <: DataStructItem], cellHeight: Int): Unit = {
-        
+
         val headerDiv = jQuery("<div>", new js.Object {
             val `class` = "ganttview-vtheader"
         })
 
-        isc debugTrap headerDiv
+        //isc debugTrap headerDiv
 
         data.toSeqZipWithIndex.foreach {
             case (dataStructItem, i) ⇒
@@ -60,30 +66,32 @@ class Chart(div: JQuery, opts: GanttChartOptions) extends js.Object {
                     val `class` = "ganttview-vtheader-item"
                 })
 
-                isc debugTrap itemDiv
+                //isc debugTrap itemDiv
 
                 dataStructItem.name.foreach {
                     name ⇒
-                        isc debugTrap name
+                        //isc debugTrap name
+                        if (name.trim.nonEmpty) {
+                            val _css = new js.Object {
+                                val height = (dataStructItem.series.length * cellHeight) + "px"
+                            }
 
-                        if (name.trim.nonEmpty)
                             itemDiv.append(jQuery("<div>", new js.Object {
                                 val `class` = "ganttview-vtheader-item-name"
-                                val `css` = new js.Object {
-                                    val `height` = (dataStructItem.series.length * cellHeight) + "px"
-                                }
+                                val css = _css
                             }).append(name))
+                        }
                 }
 
                 val seriesDiv = jQuery("<div>", new js.Object {
                     val `class` = "ganttview-vtheader-series"
                 })
 
-                isc debugTrap seriesDiv
+                //isc debugTrap seriesDiv
 
                 dataStructItem.series.toSeqZipWithIndex.foreach {
                     case (ganttDataItem, j) ⇒
-                        isc debugTrap ganttDataItem
+                        //isc debugTrap ganttDataItem
 
                         seriesDiv.append(jQuery("<div>", new js.Object {
                             val `class` = "ganttview-vtheader-series-name"
@@ -94,7 +102,7 @@ class Chart(div: JQuery, opts: GanttChartOptions) extends js.Object {
                 headerDiv append itemDiv
         }
 
-        isc debugTrap headerDiv
+        //isc debugTrap headerDiv
         div append headerDiv
     }
 }
