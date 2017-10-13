951a1b259b50bfd33bac69bee236256e008b483c
andrew <uandrew1965@gmail.com>
2017-10-13 18:18:26 +0300

    .

--- web-ui/src/main/scala/com/simplesys/app/http/TestDiagramPage.scala
+++ web-ui/src/main/scala/com/simplesys/app/http/TestDiagramPage.scala
@@ -41,8 +41,8 @@ object TestDiagramPage extends WebTabSetApp {
     override protected val formItemsJS_admin_UserGroup_FRMITM: Seq[FormItem] = FormItemsJS.admin_UserGroup_FRMITM
     override protected val formItemsJS_admin_User_FRMITM: Seq[FormItem] = FormItemsJS.admin_User_FRMITM
 
-    override protected val admin_User_codeGroup_NameStrong: NameStrong = ScalaJSGen.admin_User_codeGroup_group_NameStrong
-    override protected val admin_User_captionGroup_NameStrong: NameStrong = ScalaJSGen.admin_User_captionGroup_group_NameStrong
+    override protected val admin_User_codeGroup_NameStrong: NameStrong = ScalaJSGen.admin_User_codeGroup_NameStrong
+    override protected val admin_User_captionGroup_NameStrong: NameStrong = ScalaJSGen.admin_User_captionGroup_NameStrong
 
     override protected def getSettingsEditor(): SettingsEditor = SettingsEditor.create(
         new SettingsEditorProps {
