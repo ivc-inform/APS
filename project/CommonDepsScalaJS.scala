8019890fa611d7887e553949ad0da7214443be2c
andrew <uandrew1965@gmail.com>
2017-11-07 15:44:21 +0300

    .

--- project/CommonDepsScalaJS.scala
+++ project/CommonDepsScalaJS.scala
@@ -5,13 +5,13 @@ import sbt._
 
 object CommonDepsScalaJS {
 
-    val macroJS = Def.setting("com.simplesys" %%% "macrojs" % CommonDeps.scalaJSVersion)
-    //val smartClientWrapper = Def.setting("com.simplesys" %%% "smartclient-wrapper" % CommonDeps.scalaJSVersion)
-    val smartClientWrapper = Def.setting("com.simplesys" %%% "common-types" % CommonDeps.scalaJSVersion)
+    val macroJS = Def.setting("com.simplesys" %%% "macrojs" % CommonDeps.versions.scalaJSVersion)
+    //val smartClientWrapper = Def.setting("com.simplesys" %%% "smartclient-wrapper" % CommonDeps.versions.scalaJSVersion)
+    val smartClientWrapper = Def.setting("com.simplesys" %%% "common-types" % CommonDeps.versions.scalaJSVersion)
 
-    //val uPickleJS = Def.setting("com.lihaoyi" %%% "upickle" % CommonDeps.uPickleVersion)
-    val scalaTags = Def.setting("com.lihaoyi" %%% "scalatags" % CommonDeps.scalaTagsVersion)
-    val scalaDom = Def.setting("org.scala-js" %%% "scalajs-dom" % CommonDeps.scalajsDOMVersion)
-    //val jQuery = Def.setting("be.doeraene" %%% "scalajs-jquery" % CommonDeps.jQueryVersion)
-    val jQuery = Def.setting("io.udash" %%% "udash-jquery" % CommonDeps.udashJQueryVersion)
+    //val uPickleJS = Def.setting("com.lihaoyi" %%% "upickle" % CommonDeps.versions.uPickleVersion)
+    val scalaTags = Def.setting("com.lihaoyi" %%% "scalatags" % CommonDeps.versions.scalaTagsVersion)
+    val scalaDom = Def.setting("org.scala-js" %%% "scalajs-dom" % CommonDeps.versions.scalajsDOMVersion)
+    //val jQuery = Def.setting("be.doeraene" %%% "scalajs-jquery" % CommonDeps.versions.jQueryVersion)
+    val jQuery = Def.setting("io.udash" %%% "udash-jquery" % CommonDeps.versions.udashJQueryVersion)
 }
