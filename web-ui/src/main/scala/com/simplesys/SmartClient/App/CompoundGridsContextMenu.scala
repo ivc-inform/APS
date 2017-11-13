df6730a8efdb63ff8858cd8ae6feafe032754abe
andrew <uandrew1965@gmail.com>
2017-11-13 19:26:35 +0300

    .

--- web-ui/src/main/scala/com/simplesys/SmartClient/App/CompoundGridsContextMenu.scala
+++ web-ui/src/main/scala/com/simplesys/SmartClient/App/CompoundGridsContextMenu.scala
@@ -2,19 +2,21 @@ package com.simplesys.SmartClient.App
 
 import com.simplesys.SmartClient.Control.menu.MenuSSItem
 import com.simplesys.SmartClient.Control.{AbstractMenuSSCompanion, MenuSS}
+import com.simplesys.SmartClient.Grids.{ListGrid, ListGridEditor}
 import com.simplesys.option.{ScNone, ScOption}
 
 import scala.scalajs.js
 import scala.scalajs.js.|
 
 trait GridContextMenuData extends js.Object {
-    val simpleTable: js.UndefOr[Boolean] = js.undefined
+    val grid: js.UndefOr[ListGridEditor] = js.undefined
     val captionMenu: String
     val customMenu: js.UndefOr[Seq[MenuSSItem]] = js.undefined
 }
 
 @js.native
 trait CompoundGridsContextMenu extends MenuSS {
+    val gridsContextMenuData: Seq[GridContextMenuData]
 }
 
 @js.native
--- web-ui/src/main/scala/com/simplesys/SmartClient/App/props/CompoundGridsContextMenuProps.scala
+++ web-ui/src/main/scala/com/simplesys/SmartClient/App/props/CompoundGridsContextMenuProps.scala
@@ -1,10 +1,11 @@
 package com.simplesys.SmartClient.App.props
 
 import com.simplesys.SmartClient.App.{CompoundGridsContextMenu, GridContextMenuData, TreeListGridContextMenu}
-import com.simplesys.SmartClient.Control.props.MenuSSProps
+import com.simplesys.SmartClient.Control.props.{ListGridContextMenuProps, ListGridContextMenuWithFormProps, MenuSSProps, TreeGridContextMenuProps}
 import com.simplesys.SmartClient.System._
 import com.simplesys.System._
 import com.simplesys.function._
+import com.simplesys.option.ScNone
 import com.simplesys.option.ScOption._
 
 import scala.scalajs.js._
@@ -12,12 +13,46 @@ import scala.scalajs.js._
 class CompoundGridsContextMenuProps extends MenuSSProps {
     type classHandler <: CompoundGridsContextMenu
 
-    var grids : Option[Seq[GridContextMenuData]] = None
+    var gridsContextMenuData: Option[Seq[GridContextMenuData]] = None
 
     initWidget = {
-        (thiz: classHandler, arguments: IscArray[JSAny]) =>
-            //isc debugTrac(thiz.getClassName(), thiz.getIdentifier())
+        (thizTop: classHandler, arguments: IscArray[JSAny]) =>
+            thizTop.Super("initWidget", arguments)
 
-            thiz.Super("initWidget", arguments)
+            thizTop.gridsContextMenuData.foreach {
+                gridContextMenuData ⇒
+                    def listGridEditorMenu = ListGridContextMenu.create(
+                        new ListGridContextMenuProps {
+                            customMenu = if (gridContextMenuData.customMenu.isEmpty) ScNone else gridContextMenuData.customMenu.get.toSeq.opt
+                            owner = gridContextMenuData.grid.opt
+                        }
+                    )
+
+                    def listGridEditorMenuWithForm = ListGridContextMenuWithForm.create(
+                        new ListGridContextMenuWithFormProps {
+                            customMenu = if (gridContextMenuData.customMenu.isEmpty) ScNone else gridContextMenuData.customMenu.get.toSeq.opt
+                            owner = gridContextMenuData.grid.opt
+                        }
+                    )
+
+                    def treeGridEditorMenu = TreeGridContextMenu.create(
+                        new TreeGridContextMenuProps {
+                            customMenu = if (gridContextMenuData.customMenu.isEmpty) ScNone else gridContextMenuData.customMenu.get.toSeq.opt
+                            owner = gridContextMenuData.grid.opt
+                        }
+                    )
+
+                    gridContextMenuData.grid.foreach {
+                        grid ⇒
+                            if (isc.isA.ListGridEditor(grid)) {
+                                if (grid.simpleTable.getOrElse(false))
+                                    grid setContextMenu listGridEditorMenu
+                                else
+                                    grid setContextMenu listGridEditorMenuWithForm
+                            } else if (isc.isA.TreeGridEditor(grid)) {
+                                grid setContextMenu treeGridEditorMenu
+                            }
+                    }
+            }
     }.toThisFunc.opt
 }
