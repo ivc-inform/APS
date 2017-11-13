48bcc0d569ea998f4d846f4d6a96153a73c34158
andrew <uandrew1965@gmail.com>
2017-11-13 15:43:22 +0300

    .

--- web-ui/src/main/scala/com/simplesys/js/components/asp/props/TasksLayoutProps.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/asp/props/TasksLayoutProps.scala
@@ -1,12 +1,16 @@
 package com.simplesys.js.components.asp.props
 
+import com.simplesys.SmartClient.Layout.props.tabSet.TabProps
 import com.simplesys.SmartClient.Layout.props.{ChainMasterDetailProps, HLayoutSSProps, TabSetSSProps}
+import com.simplesys.SmartClient.System.Tab
 import com.simplesys.SmartClient.System._
 import com.simplesys.System._
-import com.simplesys.app.Tasks
+import com.simplesys.app
+import com.simplesys.app.{OpersType, Tasks}
 import com.simplesys.function._
 import com.simplesys.js.components.asp.TasksLayout
 import com.simplesys.option.ScOption._
+import com.simplesys.option.DoubleType._
 
 import scala.scalajs.js._
 
@@ -19,8 +23,9 @@ class TasksLayoutProps extends ChainMasterDetailProps {
         (thiz: classHandler, args: IscArray[JSAny]) ⇒
             thiz.Super("initWidget", args)
 
-            val tasks = Tasks.create(new TasksProps{
+            val tasks = Tasks.create(new TasksProps {
                 identifier = s"${thiz.identifier}_tasks".opt
+                width = "20%"
             })
             tasks.showResizeBar = true
             thiz addMember tasks
@@ -28,6 +33,18 @@ class TasksLayoutProps extends ChainMasterDetailProps {
             val tabSet = TabSetSS.create(
                 new TabSetSSProps {
                     identifier = s"${thiz.identifier}_tabSet".opt
+                    width = "*"
+                    canCloseTabs = false.opt
+                    tabs = Seq(
+                        Tab(
+                            new TabProps {
+                                name = "rc".opt
+                                pane = OpersType.create(new OpersTypeProps).opt
+                                title = "Типы операций".opt
+                                icon = app.doctypes.opt
+                            }
+                        )
+                    ).opt
                 }
             )
             thiz addMember tabSet
