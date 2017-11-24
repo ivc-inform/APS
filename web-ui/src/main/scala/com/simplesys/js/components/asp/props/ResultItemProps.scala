197ad14642f0eb9f67824433856ece5073d9e652
andrew <uandrew1965@gmail.com>
2017-11-24 16:44:46 +0300

    .

--- web-ui/src/main/scala/com/simplesys/js/components/asp/props/ResultItemProps.scala
+++ web-ui/src/main/scala/com/simplesys/js/components/asp/props/ResultItemProps.scala
@@ -58,7 +58,6 @@ class ResultItemProps extends CommonListGridEditorComponentProps {
             nameStrong = aps_result_items_pos_NameStrong.opt
             align = Alignment.center.opt
             `type` = ListGridFieldType.nInt_SimpleType.opt
-            canEdit = false.opt
         },
         new ListGridFieldProps {
             nameStrong = aps_result_items_duration_NameStrong.opt
