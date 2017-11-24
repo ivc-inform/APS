08030ac3a3d910bd6b9200bdb27465b7c3760228
andrew <uandrew1965@gmail.com>
2017-11-24 14:54:05 +0300

    .

--- web-ui/src/main/scala/com/simplesys/SmartClient/Forms/formsItems/props/PasswordItemProps.scala
+++
@@ -1,7 +0,0 @@
-package com.simplesys.SmartClient.Forms.formsItems.props
-
-import com.simplesys.System.Types.FormItemComponentType
-
-class PasswordItemProps extends FormItemProps {
-    `type` = FormItemComponentType.PasswordItem
-}
