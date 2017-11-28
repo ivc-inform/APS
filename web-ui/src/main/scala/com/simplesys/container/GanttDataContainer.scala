d765e7e34f1f32ead5808987a0787dce98d2be57
andrew <uandrew1965@gmail.com>
2017-11-28 18:18:01 +0300

    .

--- web-ui/src/main/scala/com/simplesys/container/GanttDataContainer.scala
+++ web-ui/src/main/scala/com/simplesys/container/GanttDataContainer.scala
@@ -4,7 +4,7 @@ import com.simplesys.SQL.Gen.AscOrderBy
 import com.simplesys.app.SessionContextSupport
 import com.simplesys.circe.Circe._
 import com.simplesys.common.Strings.newLine
-import com.simplesys.gantt.Group
+import com.simplesys.gantt.{Group, Opening}
 import com.simplesys.gantt.JVM.TaskItemExt
 import com.simplesys.isc.dataBinging.{DSRequest, DSResponse, DSResponseFailureEx, RPCResponse}
 import com.simplesys.jdbc.control.classBO.{OrderBy, Where}
@@ -18,6 +18,9 @@ import io.circe.generic.auto._
 import io.circe.syntax._
 import ru.simplesys.defs.bo.aps.{ResultDS, Result_itemsDS}
 import com.simplesys.gantt.TaskCssClass.ggroupblack
+import com.simplesys.gantt.GanttChartCommon._
+import com.simplesys.jdbc._
+import com.simplesys.common.array._
 //import com.simplesys.gantt.JVM.TaskItemExt._ !! Must be
 import com.simplesys.gantt.JVM.TaskItemExt._
 
@@ -44,19 +47,35 @@ class GanttDataContainer(val request: HttpServletRequest, val response: HttpServ
                             val taskGroupItem = TaskItemExt(
                                 pID = result.idresultResult,
                                 pName = result.scodeResult,
+                                pRes = Some(result.scodeResult),
                                 pClass = ggroupblack,
                                 pGroup = Some(Group.standardGroupTask)
                             )
+                            var prevID: Long = taskGroupItem.pID
+
                             dataSetResultItems.selectPList(where = Where(dataSetResultItems.id_resultResult_items === value.idResult), orderBy = OrderBy(dataSetResultItems.posResult_items, AscOrderBy)).result match {
                                 case Success(resultItems) ⇒
                                     Out(
                                         DSResponse(
                                             data = arr(
-                                                TaskItemExt(
-                                                    pID = result.idresultResult,
-                                                    pName = "",
-                                                    pClass = ggroupblack
-                                                ).asJson
+                                                resultItems.map {
+                                                    resultItem ⇒
+                                                       val res = TaskItemExt(
+                                                            pID = resultItem.id_itemResult_items,
+                                                            pStart = resultItem.opertimestartResult_items,
+                                                            pEnd = resultItem.opertimeendResult_items,
+                                                            pName = resultItem.scodeResult_Id_result,
+                                                            pRes = Some(result.scodeResult),
+                                                            pComp = Some(100),
+                                                            pParent = Some(result.idresultResult),
+                                                            pOpen = Some(Opening.open),
+                                                            pClass = ggroupblack, //?
+                                                            pDepend = Some(prevID.FS)
+                                                        ).asJson
+
+                                                        prevID = resultItem.id_itemResult_items
+                                                        res
+                                                }: _*
                                             ),
                                             status = RPCResponse.statusSuccess.hashCode()
                                         )
