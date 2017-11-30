81d92003b0c5d7df011c0b163e3c568f7f8b57d0
andrew <uandrew1965@gmail.com>
2017-11-30 19:13:59 +0300

    .

--- webUI/.js/src/main/scala/com/simplesys/js/components/gantt/props/GanttImprovedChartProps.scala
+++ webUI/.js/src/main/scala/com/simplesys/js/components/gantt/props/GanttImprovedChartProps.scala
@@ -3,21 +3,28 @@ package com.simplesys.js.components.gantt.props
 
 import com.simplesys.SmartClient.Foundation.props.CanvasProps
 import com.simplesys.SmartClient.RPC.props.RPCRequestProps
-import com.simplesys.SmartClient.RPC.{RPCManagerSS, RPCRequest, RPCResponse}
-import com.simplesys.SmartClient.System.{IscArray, RPCRequest}
-import com.simplesys.System.{JSAny, JSObject, JSUndefined}
+import com.simplesys.SmartClient.RPC.{RPCManagerSS, RPCRequest, RPCResponse, RPCResponseStatic}
+import com.simplesys.SmartClient.System.{IscArray, RPCRequest, isc}
+import com.simplesys.System.{JSAny, JSUndefined}
 import com.simplesys.function._
+import com.simplesys.gantt.TaskItemExt
 import com.simplesys.js.components.gantt.GanttImprovedChart
 import com.simplesys.option.DoubleType._
 import com.simplesys.option.ScOption._
 import com.simplesys.option.{ScNone, ScOption}
-//import io.circe.scalajs._
+import com.simplesys.request.RequestResult
+import io.circe.generic.auto._
+import io.circe.scalajs._
+import io.circe.syntax._
+
 import scala.language.implicitConversions
 import scalatags.Text.all._
-
+//import com.simplesys.gantt.TaskItemExt._ //!! Must be //!! Must be
+import com.simplesys.gantt.TaskItemExt._ //!! Must be //!! Must be
 
 class GanttImprovedChartProps extends CanvasProps {
     type classHandler <: GanttImprovedChart
+    val EOL = "\n"
 
     height = "100%"
     getID1 = {
@@ -47,19 +54,27 @@ class GanttImprovedChartProps extends CanvasProps {
                 thiz
             else {
                 thiz.Super("draw")
-                //isc info s"idResult: ${thiz.idResult.getOrElse(0.0)}"
+                //isc info s"idResult: ${isc.JSON.encode(convertJsonToJs(RequestResult(thiz.idResult.getOrElse(0.0).toLong).asJson).asInstanceOf[js.Object])}"
 
                 RPCManagerSS.sendRequest(
                     RPCRequest(
                         new RPCRequestProps {
                             actionURL = "logic/getGanttData".opt
-                            //data = convertJsonToJs(RequestResult(idResult = thiz.idResult.getOrElse(0.0).toLong).asJson).opt
+                            data = convertJsonToJs(RequestResult(idResult = thiz.idResult.getOrElse(0.0).toLong).asJson).opt
                             timeout = 60000.opt
                             sendNoQueue = true.opt
                             callback = {
-                                (resp: RPCResponse, data: JSObject, req: RPCRequest) ⇒
-                                    if (resp.httpResponseCode == 200) {
+                                (resp: RPCResponse, data: JSAny, req: RPCRequest) ⇒
+                                    if (resp.httpResponseCode == RPCResponseStatic.STATUS_SUCCESS) {
+                                        convertJsToJson(data) match {
+                                            case Right(json) ⇒
+                                                json.as[Seq[TaskItemExt]].foreach(println)
 
+                                                
+
+                                            case Left(failure) ⇒
+                                                isc errorDetail(failure.getMessage, failure.getStackTrace.mkString(EOL, EOL, EOL))
+                                        }
                                     }
 
                             }.toFunc.opt
--- webUI/.jvm/src/main/scala/com/simplesys/container/GanttDataContainer.scala
+++ webUI/.jvm/src/main/scala/com/simplesys/container/GanttDataContainer.scala
@@ -7,28 +7,26 @@ import com.simplesys.circe.Circe._
 import com.simplesys.common.Strings.newLine
 import com.simplesys.common.array._
 import com.simplesys.gantt.GanttChartCommon._
-import com.simplesys.gantt.JVM.TaskItemExt
-import com.simplesys.gantt.TaskCssClass.ggroupblack
+import com.simplesys.gantt.TaskItemExt
+import com.simplesys.gantt.TaskCssClass._
 import com.simplesys.gantt.{Group, Opening}
-import com.simplesys.isc.dataBinging.{DSRequest, DSResponse, DSResponseFailureEx, RPCResponse}
+import com.simplesys.isc.dataBinging.{DSResponse, DSResponseFailureEx, RPCResponse}
 import com.simplesys.jdbc.control.classBO.{OrderBy, Where}
 import com.simplesys.request.RequestResult
 import com.simplesys.servlet.ServletContext
 import com.simplesys.servlet.http.{HttpServletRequest, HttpServletResponse}
 import com.simplesys.servlet.isc.{GetData, ServletActor}
-import io.circe.Json
 import io.circe.Json._
 import io.circe.generic.auto._
 import io.circe.syntax._
 import ru.simplesys.defs.bo.aps.{ResultDS, Result_itemsDS}
-//import com.simplesys.gantt.JVM.TaskItemExt._ //!! Must be
-import com.simplesys.gantt.JVM.TaskItemExt._ //!! Must be
+//import com.simplesys.gantt.TaskItemExt._ //!! Must be
+import com.simplesys.gantt.TaskItemExt._ //!! Must be
 import scalaz.{Failure, Success}
 
 @RSTransfer(urlPattern = "/logic/getGanttData")
 class GanttDataContainer(val request: HttpServletRequest, val response: HttpServletResponse, val servletContext: ServletContext) extends ServletActor with SessionContextSupport {
 
-    val requestData: DSRequest = request.JSONData.as[DSRequest].getOrElse(throw new RuntimeException("Dont parsed Request JSON"))
     val dataSetResultItems = Result_itemsDS(oraclePool)
     val dataSetResult = ResultDS(oraclePool)
     val EOL = util.Properties.lineSeparator
@@ -36,12 +34,12 @@ class GanttDataContainer(val request: HttpServletRequest, val response: HttpServ
     def receive = {
 
         case GetData => {
-            logger debug s"request: ${newLine + requestData.asJson.toPrettyString}"
+            val requestData = request.JSONData.as[RequestResult]
 
-            val data = requestData.data.getOrElse(Json.Null)
-            logger debug s"data: ${newLine + data.toPrettyString}"
-            val resultRequest = data.as[RequestResult] match {
+            val resultRequest = requestData match {
                 case Right(value) ⇒
+                    logger debug s"request: ${newLine + value.asJson.toPrettyString}"
+
                     dataSetResult.selectPOne(where = Where(dataSetResult.idresultResult === value.idResult)).result match {
                         case Success(result) ⇒
                             val taskGroupItem = TaskItemExt(
@@ -60,16 +58,16 @@ class GanttDataContainer(val request: HttpServletRequest, val response: HttpServ
                                             data = arr(
                                                 resultItems.map {
                                                     resultItem ⇒
-                                                       val res = TaskItemExt(
+                                                        val res = TaskItemExt(
                                                             pID = resultItem.id_itemResult_items,
-                                                            pStart = resultItem.opertimestartResult_items,
-                                                            pEnd = resultItem.opertimeendResult_items,
+                                                            pStart = resultItem.opertimestartResult_items.map(item ⇒ com.simplesys.gantt.time.Time.localDateTime2Str(item)),
+                                                            pEnd = resultItem.opertimeendResult_items.map(item ⇒ com.simplesys.gantt.time.Time.localDateTime2Str(item)),
                                                             pName = resultItem.scodeResult_Id_result,
                                                             pRes = Some(result.scodeResult),
                                                             pComp = Some(100),
                                                             pParent = Some(result.idresultResult),
                                                             pOpen = Some(Opening.open),
-                                                            pClass = ggroupblack, //?
+                                                            pClass = if (resultItem.idchangeoverChangeover_Id_changeover.isEmpty) gtaskblue else gtaskyellow,
                                                             pDepend = Some(prevID.FS)
                                                         ).asJson
 
@@ -77,7 +75,7 @@ class GanttDataContainer(val request: HttpServletRequest, val response: HttpServ
                                                         res
                                                 }: _*
                                             ),
-                                            status = RPCResponse.statusSuccess.hashCode()
+                                            status = RPCResponse.statusSuccess
                                         )
                                     )
                                 case
@@ -85,7 +83,7 @@ class GanttDataContainer(val request: HttpServletRequest, val response: HttpServ
                                     Out(
                                         DSResponseFailureEx(
                                             failure.getMessage,
-                                            failure.getStackTrace().mkString("", EOL, EOL)).asJson
+                                            failure.getStackTrace.mkString("", EOL, EOL)).asJson
                                     )
                             }
 
@@ -93,17 +91,17 @@ class GanttDataContainer(val request: HttpServletRequest, val response: HttpServ
                             Out(
                                 DSResponseFailureEx(
                                     failure.getMessage,
-                                    failure.getStackTrace().mkString("", EOL, EOL)).asJson
+                                    failure.getStackTrace.mkString("", EOL, EOL)).asJson
                             )
                     }
                 case Left(failure) ⇒
                     Out(DSResponseFailureEx(
                         failure.message,
-                        failure.getStackTrace().mkString("", EOL, EOL)).asJson
+                        failure.getStackTrace.mkString("", EOL, EOL)).asJson
                     )
             }
 
-            selfStop()
+            selfStop
         }
         case x =>
             throw new RuntimeException(s"Bad branch $x")
