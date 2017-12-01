f355a930f31b48001af268aae545acf8fa8f948f
andrew <uandrew1965@gmail.com>
2017-12-01 18:18:16 +0300

    .

--- webUI/.js/src/main/scala/com/simplesys/js/components/gantt/props/GanttImprovedChartProps.scala
+++ webUI/.js/src/main/scala/com/simplesys/js/components/gantt/props/GanttImprovedChartProps.scala
@@ -3,12 +3,10 @@ package com.simplesys.js.components.gantt.props
 
 import com.simplesys.SmartClient.Foundation.props.CanvasProps
 import com.simplesys.SmartClient.RPC.props.RPCRequestProps
-import com.simplesys.SmartClient.RPC.{RPCManagerSS, RPCRequest, RPCResponse, RPCResponseStatic}
+import com.simplesys.SmartClient.RPC.{RPCManagerSS, RPCRequest, RPCResponse}
 import com.simplesys.SmartClient.System.{IscArray, RPCRequest, isc}
-import com.simplesys.System.{JSAny, JSArray, JSObject, JSUndefined}
+import com.simplesys.System.{JSAny, JSUndefined}
 import com.simplesys.function._
-import com.simplesys.gantt.TaskItemExt
-import com.simplesys.isc.dataBinging.DSResponse
 import com.simplesys.js.components.gantt.GanttImprovedChart
 import com.simplesys.option.DoubleType._
 import com.simplesys.option.ScOption._
@@ -20,8 +18,7 @@ import io.circe.syntax._
 
 import scala.language.implicitConversions
 import scalatags.Text.all._
-//import com.simplesys.gantt.TaskItemExt._ //!! Must be //!! Must be
-import com.simplesys.gantt.TaskItemExt._ //!! Must be //!! Must be
+//import com.simplesys.gantt.TaskItemExt._ //!! Must be //!! Must be //!! Must be //!! Must be
 
 class GanttImprovedChartProps extends CanvasProps {
     type classHandler <: GanttImprovedChart
@@ -72,7 +69,7 @@ class GanttImprovedChartProps extends CanvasProps {
                                             case Left(failure) ⇒
                                                 isc error(failure.getMessage)
                                         }
-                                        data.foreach {
+                                        /*data.foreach {
                                             item ⇒
                                                 println(item)
                                                 convertJsToJson(item) match {
@@ -86,7 +83,7 @@ class GanttImprovedChartProps extends CanvasProps {
                                                     case Left(failure) ⇒
                                                         isc errorDetail(failure.getMessage, failure.getStackTrace.mkString(EOL, EOL, EOL))
                                                 }
-                                        }
+                                        }*/
                                     }
 
                             }.toFunc.opt
