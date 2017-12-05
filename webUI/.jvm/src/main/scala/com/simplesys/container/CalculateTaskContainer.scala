2a4e2a1fbf19f1817f2159a846b6c2cbdf90d6dd
andrew <uandrew1965@gmail.com>
2017-12-05 11:46:40 +0300

    .

--- webUI/.jvm/src/main/scala/com/simplesys/container/CalculateTaskContainer.scala
+++ webUI/.jvm/src/main/scala/com/simplesys/container/CalculateTaskContainer.scala
@@ -4,11 +4,12 @@ import com.simplesys.annotation.RSTransfer
 import com.simplesys.app.SessionContextSupport
 import com.simplesys.circe.Circe._
 import com.simplesys.common.Strings.newLine
-import com.simplesys.isc.dataBinging.DSResponseFailureEx
+import com.simplesys.isc.dataBinging.{DSResponse, DSResponseFailureEx, RPCResponse}
 import com.simplesys.request.CalculateRequest
 import com.simplesys.servlet.ServletContext
 import com.simplesys.servlet.http.{HttpServletRequest, HttpServletResponse}
 import com.simplesys.servlet.isc.{GetData, ServletActor}
+import io.circe.Json.arr
 import io.circe.generic.auto._
 import io.circe.syntax._
 
@@ -25,7 +26,15 @@ class CalculateTaskContainer(val request: HttpServletRequest, val response: Http
             requestData match {
                 case Right(value) ⇒
                     logger debug s"request: ${newLine + value.asJson.toPrettyString}"
-                    
+
+                    ru.simplesys.aps.dataprovider.Processor.runIt(value.idTask, 5)
+                    Out(
+                        DSResponse(
+                            data = ,
+                            status = RPCResponse.statusSuccess
+                        )
+                    )
+
                 case Left(failure) ⇒
                     Out(DSResponseFailureEx(
                         failure.message,
